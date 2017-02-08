a675118eb10337ee4d9175517475c5bee1fa6f84
Zachery Kayser <zkayser@gmail.com>
2017-02-08 02:15:59 +0000

    Implement PrivateRoomController.create

--- web/controllers/private_room_controller.ex
+++ web/controllers/private_room_controller.ex
@@ -1,9 +1,10 @@
 defmodule PokerEx.PrivateRoomController do
   use PokerEx.Web, :controller
   alias PokerEx.PrivateRoom
+  alias PokerEx.Player
   
   def new(conn, params) do
-    changeset = PrivateRoom.changeset(%PrivateRoom{invitees: []}, %{})
+    changeset = PrivateRoom.changeset(%PrivateRoom{invitees: [], owner: nil}, %{})
     query = 
       from p in PokerEx.Player,
         where: p.id != ^conn.assigns[:current_player].id,
@@ -14,9 +15,33 @@ defmodule PokerEx.PrivateRoomController do
     render conn, "new.html", changeset: changeset, players: players
   end
   
-  def create(conn, params) do
-    IO.puts "Priv Room create params: \nParams: #{inspect(params)}"
-    conn
-    |> redirect(to: private_room_path(conn, :new))
+  def create(conn, %{"invitees" => invitees, "private_room" => %{"title" => title, "owner" => owner} = room_params} = params) do
+    private_room = %PrivateRoom{invitees: [], owner: nil, participants: []}
+    changeset = 
+      PrivateRoom.changeset(private_room, room_params)
+      |> PrivateRoom.put_owner(owner)
+      |> PrivateRoom.put_invitees(Map.values(invitees))
+    
+    case PokerEx.Repo.insert(changeset) do
+      {:ok, room} -> 
+        conn
+        |> put_flash(:info, "#{room.title} has been created")
+        |> redirect(to: private_room_path(conn, :show, room.id))
+      {:error, error_changeset} ->
+        conn
+        |> put_flash(:error, "Something went wrong")
+        |> redirect(to: private_room_path(conn, :new))
+    end
+  end
+  
+  def show(conn, %{"id" => id}) do
+    room = PokerEx.Repo.get(PrivateRoom, String.to_integer(id)) |> PrivateRoom.preload()
+    unless conn.assigns[:current_player] in room.invitees || conn.assigns[:current_player] == room.owner do
+      conn
+      |> put_flash(:error, "Access restricted")
+      |> redirect(to: player_path(conn, :show, conn.assigns[:current_player]))
+    end
+    
+    render conn, "show.html", room: room
   end
 end
--- web/models/private_room.ex
+++ web/models/private_room.ex
@@ -1,11 +1,21 @@
 defmodule PokerEx.PrivateRoom do
   use PokerEx.Web, :model
+  alias PokerEx.Player
+  alias PokerEx.Repo
   
   schema "private_rooms" do
     field :title, :string
     belongs_to :owner, PokerEx.Player
-    many_to_many :participants, PokerEx.Player, join_through: "participants_private_rooms", join_keys: [private_room_id: :id, participant_id: :id]
-    many_to_many :invitees, PokerEx.Player, join_through: "invitees_private_rooms", join_keys: [private_room_id: :id, invitee_id: :id]
+    many_to_many :participants, 
+                  PokerEx.Player, 
+                  join_through: "participants_private_rooms",
+                  join_keys: [private_room_id: :id, participant_id: :id],
+                  on_delete: :delete_all
+    many_to_many :invitees, 
+                  PokerEx.Player, 
+                  join_through: "invitees_private_rooms", 
+                  join_keys: [private_room_id: :id, invitee_id: :id],
+                  on_replace: :delete
     timestamps()
   end
   
@@ -32,6 +42,29 @@ defmodule PokerEx.PrivateRoom do
     model
     |> changeset(params)
     |> do_update_changeset(params)
+  end
+  
+  def preload(private_room) do
+    private_room |> Repo.preload([:invitees, :owner, :participants])
+  end
+  
+  def put_owner(changeset, owner) do
+    owner = 
+      case Repo.get(Player, String.to_integer(owner)) do
+        nil -> add_error(changeset, :owner, "invalid owner")
+        player -> 
+          changeset
+          |> put_assoc(:owner, player)
+      end
+  end
+  
+  def put_invitees(changeset, invitees) when is_list(invitees) do
+    invitees = 
+      Enum.map(invitees, &String.to_integer/1) 
+      |> Enum.map(&(Repo.get(Player, &1)))
+      |> Enum.reject(&(&1 == nil))
+    changeset
+    |> put_assoc(:invitees, invitees)
   end
   
   defp do_update_changeset(changeset, %{"participants" => participants, "invitees" => invitees} = params) do
