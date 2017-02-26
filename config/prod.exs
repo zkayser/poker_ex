4404383416305f5541b27ae6cf658d8647cd3f8e
Zachery Kayser <zkayser@gmail.com>
2017-02-26 08:48:21 +0000

    Minor bug fix

--- config/prod.exs
+++ config/prod.exs
@@ -21,7 +21,7 @@ config :poker_ex, PokerEx.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
-config :hello_phoenix, HelloPhoenix.Repo,
+config :poker_ex, PokerEx.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
