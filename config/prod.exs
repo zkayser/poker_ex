6b05bcd2a8cfe4742395379e209493f597bdbb12
Zachery Kayser <zkayser@gmail.com>
2017-02-26 08:30:42 +0000

    Setup for Heroku deploy

--- config/prod.exs
+++ config/prod.exs
@@ -13,20 +13,29 @@ use Mix.Config
 # which you typically run after static files are built.
 config :poker_ex, PokerEx.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [scheme: "https", host: "https://ancient-forest-15148.herokuapp.com/", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+config :hello_phoenix, HelloPhoenix.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
 # to the previous section and set your `:url` port to 443:
 #
-#     config :poker_ex, PokerEx.Endpoint,
+#    config :poker_ex, PokerEx.Endpoint,
 #       ...
-#       url: [host: "example.com", port: 443],
+#      url: [scheme: "https", host: "https://ancient-forest-15148.herokuapp.com/", port: 443],
+#      force_ssl: [rewrite_on: [:x_forwarded_proto]],
 #       https: [port: 443,
 #               keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
 #               certfile: System.get_env("SOME_APP_SSL_CERT_PATH")]
@@ -58,4 +67,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"  ----> Not needed when using environment variables on Heroku
