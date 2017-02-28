7e903d173f46d482268fc9489e2b2f4174b0ebc5
Zachery Kayser <zkayser@gmail.com>
2017-02-28 04:22:20 +0000

    Add Application stop callback

--- lib/poker_ex.ex
+++ lib/poker_ex.ex
@@ -36,4 +36,22 @@ defmodule PokerEx do
     PokerEx.Endpoint.config_change(changed, removed)
     :ok
   end
+  
+  def stop(_state) do
+    alias PokerEx.PrivateRoom
+    alias PokerEx.RoomsSupervisor, as: RoomSup
+    alias PokerEx.Repo
+    require Logger
+    
+    priv_rooms = Repo.all(PrivateRoom)
+    for priv_room <- priv_rooms do
+      proc_alias = String.to_atom(priv_room.title)
+      Logger.info "Shutting down room #{priv_room.title}"
+      if RoomSup.room_process_exists?(proc_alias) do
+        pid = Process.whereis(proc_alias)
+        Supervisor.terminate_child(RoomSup, pid)
+      end
+    end
+  end
+  
 end
