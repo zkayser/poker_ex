f2d32362ffa22a38e56be738dd5c8d4282fe35fb
Zack Kayser <zack.kayser@plxis.com>
2017-09-30 13:02:12 -0400

    Fix small bug in social networking services

--- lib/poker/services/facebook.ex
+++ lib/poker/services/facebook.ex
@@ -3,9 +3,9 @@ defmodule PokerEx.Services.Facebook do
   @app_secret System.get_env("FACEBOOK_APP_SECRET")
   @endpoint "https://graph.facebook.com/"
   @api_vsn "v2.9/"
-  
+
   @type response :: %HTTPotion.Response{}
-  
+
   @spec notify_user(%{user_id: String.t, template: String.t, return_url: String.t}) :: response
   def notify_user(%{user_id: id, template: template, return_url: url}, app_id \\ @app_id, app_secret \\ @app_secret) do
     case {app_id, app_secret} do
@@ -18,7 +18,7 @@ defmodule PokerEx.Services.Facebook do
         [body: "", headers: ["Content-Type": "application/json"]])
     end
   end
-  
+
   defp fb_credentials_exception do
     raise "app_id and app_secret are required to use the Facebook notification service"
   end
