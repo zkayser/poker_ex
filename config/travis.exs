0dabb86054b5e2ac969eddbb0e213d8dded34545
Zack Kayser <zack.kayser@plxis.com>
2017-10-01 12:10:51 -0400

    Add configuration for Travis CI environment

---
+++ config/travis.exs
@@ -0,0 +1,31 @@
+use Mix.Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :poker_ex, PokerExWeb.Endpoint,
+  http: [port: 4001],
+  server: true
+
+# Set up SQL Sandbox for Wallaby Integration tests
+config :poker_ex, :sql_sandbox, true
+
+# Save screenshot on failed Integration tests
+config :wallaby, screenshot_on_failure: false
+
+# Easy # of password hashing rounds on :comeonin
+# to speed up tests
+config :comeonin, :bcrypt_log_rounds, 4
+config :comeonin, :pbkdf2_rounds, 1
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Configure your database
+config :poker_ex, PokerEx.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "",
+  database: "poker_ex_test",
+  hostname: "localhost",
+  template: "template0",
+  pool: Ecto.Adapters.SQL.Sandbox
