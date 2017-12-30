3e644a58fb23c8627515f23d8f242f016b612fbd
Zack Kayser <zkayser@gmail.com>
2017-12-30 14:39:35 -0500

    Test that current rooms are pushed over the wire when joining PrivateRoomChannel

--- lib/poker_ex_web/channels/private_room_channel.ex
+++ lib/poker_ex_web/channels/private_room_channel.ex
@@ -1,13 +1,13 @@
 defmodule PokerExWeb.PrivateRoomChannel do
 	use Phoenix.Channel
+	require Logger
 	alias PokerEx.Player
-	alias PokerEx.PrivateRoom
 
 	def join("private_rooms:" <> player_name, _params, socket) do
 		with %Player{} = player <- Player.by_name(player_name) do
 			case player.id == socket.assigns.player_id do
 				true ->
-					send self(), %{rooms_for: player}
+					send self(), :send_rooms
 					{:ok, %{response: :success}, assign(socket, :player, player)}
 				false -> {:error, %{message: "Authentication failed"}}
 			end
@@ -15,7 +15,38 @@ defmodule PokerExWeb.PrivateRoomChannel do
 		end
 	end
 
-	def handle_info(%{rooms_for: %Player{} = player}, socket) do
+	def handle_info(:send_rooms, socket) do
+		update_and_assign_rooms(socket, 1)
 		{:noreply, socket}
+	end
+
+	defp update_and_assign_rooms(socket, page_num) do
+		player = Player.preload(socket.assigns[:player])
+		paginated_current_rooms = get_paginated_current_rooms(player, page_num)
+		paginated_invited_rooms = get_paginated_invited_rooms(player, page_num)
+
+		Logger.debug "[PrivateRoomChannel] Pushing current and invited rooms to channel client"
+		push socket, "current_rooms",
+			%{current_rooms: %{
+					rooms: paginated_current_rooms.entries,
+					page: page_num,
+					total_pages: paginated_current_rooms.total_pages
+				 },
+				invited_rooms: %{
+					rooms: paginated_invited_rooms.entries,
+					page: page_num,
+					total_pages: paginated_invited_rooms.total_pages
+				}
+			 }
+	end
+
+	defp get_paginated_current_rooms(%Player{} = player, page_num) do
+		Enum.map(player.participating_rooms, &(String.to_atom(&1.title)))
+			|> Scrivener.paginate(%Scrivener.Config{page_number: page_num, page_size: 10})
+	end
+
+	defp get_paginated_invited_rooms(%Player{} = player, page_num) do
+		Enum.map(player.invited_rooms, &(String.to_atom(&1.title)))
+			|> Scrivener.paginate(%Scrivener.Config{page_number: page_num, page_size: 10})
 	end
 end
--- test/channels/private_room_channel_test.exs
+++ test/channels/private_room_channel_test.exs
@@ -2,29 +2,45 @@ defmodule PokerExWeb.PrivateRoomChannelTest do
 	use PokerExWeb.ChannelCase
 	import PokerEx.TestHelpers
 	alias PokerExWeb.PrivateRoomChannel
+	alias PokerEx.PrivateRoom
 
 	@endpoint PokerExWeb.Endpoint
 
 	setup do
-		{socket, player, token, reply} = create_player_and_connect()
+		{socket, player, token, reply, room} = create_player_and_connect()
 
-		{:ok, socket: socket, player: player, token: token, reply: reply}
+		{:ok, socket: socket, player: player, token: token, reply: reply, room: room}
 	end
 
 	test "it join replies with `:success` when authentication is successful", context do
 		assert context.reply.response == :success
 	end
 
+	test "a `current_rooms` message is pushed on successful joins", context do
+		room_process = String.to_atom(context.room.title)
+
+		expected_current_rooms = %{rooms: [room_process], page: 1, total_pages: 1}
+
+		expected_invited_rooms = %{rooms: [], page: 1, total_pages: 0}
+
+		assert_push "current_rooms",
+			%{current_rooms: ^expected_current_rooms,
+				invited_rooms: ^expected_invited_rooms
+			 }
+	end
+
 	defp create_player_and_connect do
 		player = insert_user()
+		invited_players = for _ <- 1..4, do: insert_user()
+		{:ok, room} = PrivateRoom.create("test#{Base.encode16(:crypto.strong_rand_bytes(8))}", player, invited_players)
 		name = player.name
 		token = Phoenix.Token.sign(socket(), "user socket", player.id)
 
 		{:ok, socket} = connect(PokerExWeb.UserSocket, %{"token" => token})
 
 		with {:ok, reply, socket} <- subscribe_and_join(socket, PrivateRoomChannel, "private_rooms:" <> name) do
-			{socket, player, token, reply}
-		else {:error, reply} -> {socket, player, token, reply}
+			{socket, player, token, reply, room}
+		else {:error, reply} -> {socket, player, token, reply, room}
 		end
 	end
 end
