83b63a09a1b643a0efa1a0bcb2fd3b41e61eda56
Kayser <zkayser@gmail.com>
2018-04-25 14:34:23 -0400

    Implement call, raise, and fold in PlayerTracker

--- lib/poker/game_engine/impl/managers/player_tracker.ex
+++ lib/poker/game_engine/impl/managers/player_tracker.ex
@@ -1,9 +1,12 @@
 defmodule PokerEx.GameEngine.PlayerTracker do
   alias PokerEx.{Player, Card}
+  alias PokerEx.GameEngine.ChipManager
   @type tracker :: [String.t() | Player.t()] | []
   @type hands :: [{String.t(), [Card.t()]}] | []
   @settable_rounds [:idle, :between_rounds]
   @type phase :: :idle | :pre_flop | :flop | :turn | :river | :game_over | :between_rounds
+  @type success :: {:ok, t()}
+  @type error :: {:error, :player_did_not_call | :player_not_active}
 
   @type t :: %__MODULE__{
           active: tracker,
@@ -34,7 +37,96 @@ defmodule PokerEx.GameEngine.PlayerTracker do
     end
   end
 
+  @spec call(PokerEx.GameEngine.Impl.t(), Player.name(), ChipManager.t()) :: success() | error()
+  def call(%{player_tracker: tracker} = engine, name, chip_manager) do
+    case get_call_state(chip_manager, name) do
+      :called ->
+        {:ok, update_state(tracker, [{:update_active, name, :to_back}, {:update_called, name}])}
+
+      :all_in ->
+        {:ok, update_state(tracker, [{:update_active, name, :drop}, {:update_all_in, name}])}
+
+      :player_did_not_call ->
+        {:error, :player_did_not_call}
+    end
+  end
+
+  @spec raise(PokerEx.GameEngine.Impl.t(), Player.name(), ChipManager.t()) :: success()
+  def raise(%{player_tracker: tracker}, name, chip_manager) do
+    case get_raise_state(chip_manager, name) do
+      :all_in ->
+        {:ok,
+         update_state(tracker, [
+           {:update_active, name, :drop},
+           :clear_called,
+           {:update_all_in, name}
+         ])}
+
+      :raised ->
+        {:ok,
+         update_state(tracker, [
+           {:update_active, name, :to_back},
+           :clear_called,
+           {:update_called, name}
+         ])}
+    end
+  end
+
+  @spec fold(PokerEx.GameEngine.Impl.t(), Player.name()) :: success() | error()
+  def fold(%{player_tracker: tracker}, name) do
+    case tracker.active do
+      [player | _] when player == name ->
+        {:ok, update_state(tracker, [{:update_active, name, :drop}, {:update_folded, name}])}
+
+      _ ->
+        {:error, :player_not_active}
+    end
+  end
+
+  defp update_state(tracker, updates) do
+    Enum.reduce(updates, tracker, &update(&1, &2))
+  end
+
+  defp update({:update_active, name, :to_back}, %{active: active} = tracker) do
+    Map.put(tracker, :active, Enum.drop(active, 1) |> Kernel.++([name]))
+  end
+
+  defp update({:update_active, name, :drop}, %{active: active} = tracker) do
+    Map.put(tracker, :active, Enum.drop(active, 1))
+  end
+
+  defp update({:update_called, name}, %{called: called} = tracker) do
+    Map.put(tracker, :called, [name | called])
+  end
+
+  defp update({:update_all_in, name}, %{all_in: all_in} = tracker) do
+    Map.put(tracker, :all_in, [name | all_in])
+  end
+
+  defp update(:clear_called, tracker) do
+    %__MODULE__{tracker | called: []}
+  end
+
+  defp update({:update_folded, name}, tracker) do
+    %__MODULE__{tracker | folded: [name | tracker.folded]}
+  end
+
   defp set_active(arrangement) do
     %__MODULE__{active: Enum.map(arrangement, fn {name, _} -> name end)}
+  end
+
+  defp get_call_state(chip_manager, name) do
+    cond do
+      chip_manager.round[name] == chip_manager.to_call -> :called
+      chip_manager.chip_roll[name] == 0 -> :all_in
+      true -> :player_did_not_call
+    end
+  end
+
+  defp get_raise_state(chip_manager, name) do
+    cond do
+      chip_manager.chip_roll[name] == 0 -> :all_in
+      true -> :raised
+    end
   end
 end
---
+++ test/lib/game_engine/managers/player_tracker_test.exs
@@ -0,0 +1,119 @@
+defmodule PokerEx.PlayerTrackerTest do
+  use ExUnit.Case
+  use PokerEx.EngineCase
+  alias PokerEx.GameEngine.Impl, as: Engine
+  alias PokerEx.GameEngine.{PlayerTracker}
+
+  describe "call/3" do
+    test "moves the calling player into the list of called players", context do
+      engine =
+        Map.put(Engine.new(), :player_tracker, TestData.insert_active_players(context))
+        |> Map.put(:chips, TestData.add_200_chips_for_all(context))
+        |> Map.update(:chips, %{}, fn chips -> Map.put(chips, :to_call, 10) end)
+
+      [active_player | _] = engine.player_tracker.active
+
+      engine =
+        Map.update(engine, :chips, %{}, fn chips ->
+          Map.update(chips, :round, %{}, fn round -> Map.put(round, active_player, 10) end)
+        end)
+
+      assert {:ok, player_tracker} = PlayerTracker.call(engine, active_player, engine.chips)
+      assert active_player in player_tracker.called
+      refute hd(player_tracker.active) == active_player
+    end
+
+    test "does not move the players who have not called into the called list", context do
+      engine =
+        Map.put(Engine.new(), :player_tracker, TestData.insert_active_players(context))
+        |> Map.put(:chips, TestData.add_200_chips_for_all(context))
+        |> Map.update(:chips, %{}, fn chips -> Map.put(chips, :to_call, 10) end)
+
+      [active_player | _] = engine.player_tracker.active
+
+      assert {:error, :player_did_not_call} =
+               PlayerTracker.call(engine, active_player, engine.chips)
+    end
+
+    test "moves a player to all_in if the player runs out of chips", context do
+      engine =
+        Map.put(Engine.new(), :player_tracker, TestData.insert_active_players(context))
+        |> Map.put(:chips, TestData.add_200_chips_for_all(context))
+        |> Map.update(:chips, %{}, fn chips -> Map.put(chips, :to_call, 250) end)
+
+      [active_player | _] = engine.player_tracker.active
+
+      engine =
+        Map.update(engine, :chips, %{}, fn chips ->
+          Map.update(chips, :round, %{}, fn round -> Map.put(round, active_player, 200) end)
+          |> Map.update(:chip_roll, %{}, fn chip_roll -> Map.put(chip_roll, active_player, 0) end)
+        end)
+
+      assert {:ok, player_tracker} = PlayerTracker.call(engine, active_player, engine.chips)
+      assert active_player in player_tracker.all_in
+    end
+  end
+
+  describe "raise/3" do
+    test "clears the called list and inserts the raising player", context do
+      engine =
+        Map.put(Engine.new(), :player_tracker, TestData.insert_active_players(context))
+        |> Map.put(:chips, TestData.add_200_chips_for_all(context))
+
+      [active_player | _] = engine.player_tracker.active
+
+      assert {:ok, player_tracker} = PlayerTracker.raise(engine, active_player, engine.chips)
+      assert [active_player] == player_tracker.called
+    end
+
+    test "places the raising player at the end of the active player list", context do
+      engine =
+        Map.put(Engine.new(), :player_tracker, TestData.insert_active_players(context))
+        |> Map.put(:chips, TestData.add_200_chips_for_all(context))
+
+      [active_player | _] = engine.player_tracker.active
+
+      assert {:ok, player_tracker} = PlayerTracker.raise(engine, active_player, engine.chips)
+      assert active_player == List.last(player_tracker.active)
+    end
+
+    test "places the raising player in all_in when using all chips", context do
+      engine =
+        Map.put(Engine.new(), :player_tracker, TestData.insert_active_players(context))
+        |> Map.put(:chips, TestData.add_200_chips_for_all(context))
+
+      [active_player | _] = engine.player_tracker.active
+
+      engine =
+        Map.update(engine, :chips, %{}, fn chips ->
+          Map.update(chips, :chip_roll, %{}, fn chip_roll ->
+            Map.put(chip_roll, active_player, 0)
+          end)
+        end)
+
+      assert {:ok, player_tracker} = PlayerTracker.raise(engine, active_player, engine.chips)
+      assert active_player in player_tracker.all_in
+      refute active_player in player_tracker.active
+    end
+  end
+
+  describe "fold/2" do
+    test "moves the folding player from active to folded", context do
+      engine = Map.put(Engine.new(), :player_tracker, TestData.insert_active_players(context))
+
+      [active_player | _] = engine.player_tracker.active
+
+      {:ok, player_tracker} = PlayerTracker.fold(engine, active_player)
+      assert active_player in player_tracker.folded
+      refute active_player in player_tracker.active
+    end
+
+    test "requires the player to be active", context do
+      engine = Map.put(Engine.new(), :player_tracker, TestData.insert_active_players(context))
+
+      non_active_player = Enum.drop(engine.player_tracker.active, 1) |> hd()
+
+      assert {:error, :player_not_active} = PlayerTracker.fold(engine, non_active_player)
+    end
+  end
+end
