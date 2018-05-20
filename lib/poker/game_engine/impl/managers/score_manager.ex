6c765454cf0c1d5e3e4343a0b02b8c07242c73a2
Kayser <zkayser@gmail.com>
2018-05-20 13:09:23 -0400

    Add GameState protocol implementation for ScoreManager

--- lib/poker/game_engine/impl/managers/score_manager.ex
+++ lib/poker/game_engine/impl/managers/score_manager.ex
@@ -1,5 +1,6 @@
 defmodule PokerEx.GameEngine.ScoreManager do
-  alias PokerEx.{Player, Hand, Evaluator, RewardManager, Events}
+  alias PokerEx.{Player, Hand}
+  alias PokerEx.GameEngine.GameState
   @type stats :: [{String.t(), pos_integer()}] | []
   @type rewards :: [{String.t(), pos_integer()}] | []
 
@@ -35,14 +36,14 @@ defmodule PokerEx.GameEngine.ScoreManager do
       true ->
         cond do
           length(engine.player_tracker.all_in) == 1 && length(engine.player_tracker.active) == 0 ->
-            update_state(%{scoring | game_id: engine.game_id}, [
+            GameState.update(%{scoring | game_id: engine.game_id}, [
               {:auto_win, engine.player_tracker.all_in, :raise},
               {:set_rewards, engine.chips},
               :set_winners
             ])
 
           length(engine.player_tracker.all_in) == 0 ->
-            update_state(%{scoring | game_id: engine.game_id}, [
+            GameState.update(%{scoring | game_id: engine.game_id}, [
               {:auto_win, engine.player_tracker.active, :fold},
               {:set_rewards, engine.chips},
               :set_winners
@@ -53,7 +54,7 @@ defmodule PokerEx.GameEngine.ScoreManager do
         end
 
       false ->
-        update_state(%{scoring | game_id: engine.game_id}, [
+        GameState.update(%{scoring | game_id: engine.game_id}, [
           {:evaluate_hands, cards},
           {:set_rewards, engine.chips},
           :set_winners,
@@ -61,82 +62,5 @@ defmodule PokerEx.GameEngine.ScoreManager do
           :send_winner_message
         ])
     end
-  end
-
-  def update_state(scoring, updates) when is_list(updates) do
-    Enum.reduce(updates, scoring, &update(&1, &2))
-  end
-
-  defp update({:evaluate_hands, cards}, scoring) do
-    hands =
-      Enum.map(cards.player_hands, &{&1.player, Evaluator.evaluate_hand(&1.hand, cards.table)})
-
-    Map.put(scoring, :stats, Enum.map(hands, fn {player, hand} -> {player, hand.score} end))
-    |> Map.put(:hands, Enum.map(hands, fn {_, hand} -> hand end))
-  end
-
-  defp update({:set_rewards, chips}, scoring) do
-    Map.put(scoring, :rewards, RewardManager.manage_rewards(scoring.stats, chips.paid))
-  end
-
-  defp update(:set_winners, scoring) do
-    {_, high_score} = Enum.max_by(scoring.stats, fn {_, score} -> score end)
-
-    winners =
-      Enum.filter(scoring.stats, fn {_, score} -> score == high_score end)
-      |> Enum.map(fn {player, _} -> player end)
-
-    Enum.each(winners, fn winner ->
-      Events.game_over(
-        scoring.game_id,
-        winner,
-        Enum.filter(scoring.rewards, fn {name, _} ->
-          winner == name
-        end)
-        |> hd()
-        |> elem(1)
-      )
-    end)
-
-    %__MODULE__{scoring | winners: winners}
-  end
-
-  defp update({:set_winning_hand, cards}, scoring) do
-    winning_hand =
-      Enum.filter(cards.player_hands, fn data ->
-        data.player == hd(scoring.winners)
-      end)
-      |> hd()
-
-    %__MODULE__{scoring | winning_hand: winning_hand}
-  end
-
-  defp update({:auto_win, [player | _], type}, scoring) do
-    message =
-      case type do
-        :raise -> "#{player} wins the round"
-        :fold -> "#{player} wins the round on a fold"
-      end
-
-    Events.winner_message(scoring.game_id, message)
-    Map.put(scoring, :stats, [{player, 1000}])
-  end
-
-  defp update(:send_winner_message, scoring) do
-    winning_hand =
-      Enum.filter(scoring.hands, fn hand ->
-        {_, score} = Enum.max_by(scoring.stats, fn {_, score} -> score end)
-        hand.score == score
-      end)
-      |> hd()
-
-    Events.present_winning_hand(
-      scoring.game_id,
-      winning_hand.best_hand,
-      hd(scoring.winners),
-      winning_hand.type_string
-    )
-
-    %{scoring | winning_hand: winning_hand}
   end
 end
---
+++ lib/poker/game_engine/impl/state_management/score_manager.ex
@@ -0,0 +1,81 @@
+defimpl PokerEx.GameEngine.GameState, for: PokerEx.GameEngine.ScoreManager do
+  alias PokerEx.{Evaluator, RewardManager, Events}
+  alias PokerEx.GameEngine.ScoreManager
+
+  def update(scoring, updates) when is_list(updates) do
+    Enum.reduce(updates, scoring, &do_update(&1, &2))
+  end
+
+  defp do_update({:evaluate_hands, cards}, scoring) do
+    hands =
+      Enum.map(cards.player_hands, &{&1.player, Evaluator.evaluate_hand(&1.hand, cards.table)})
+
+    Map.put(scoring, :stats, Enum.map(hands, fn {player, hand} -> {player, hand.score} end))
+    |> Map.put(:hands, Enum.map(hands, fn {_, hand} -> hand end))
+  end
+
+  defp do_update({:set_rewards, chips}, scoring) do
+    Map.put(scoring, :rewards, RewardManager.manage_rewards(scoring.stats, chips.paid))
+  end
+
+  defp do_update(:set_winners, scoring) do
+    {_, high_score} = Enum.max_by(scoring.stats, fn {_, score} -> score end)
+
+    winners =
+      Enum.filter(scoring.stats, fn {_, score} -> score == high_score end)
+      |> Enum.map(fn {player, _} -> player end)
+
+    Enum.each(winners, fn winner ->
+      Events.game_over(
+        scoring.game_id,
+        winner,
+        Enum.filter(scoring.rewards, fn {name, _} ->
+          winner == name
+        end)
+        |> hd()
+        |> elem(1)
+      )
+    end)
+
+    %ScoreManager{scoring | winners: winners}
+  end
+
+  defp do_update({:set_winning_hand, cards}, scoring) do
+    winning_hand =
+      Enum.filter(cards.player_hands, fn data ->
+        data.player == hd(scoring.winners)
+      end)
+      |> hd()
+
+    %ScoreManager{scoring | winning_hand: winning_hand}
+  end
+
+  defp do_update({:auto_win, [player | _], type}, scoring) do
+    message =
+      case type do
+        :raise -> "#{player} wins the round"
+        :fold -> "#{player} wins the round on a fold"
+      end
+
+    Events.winner_message(scoring.game_id, message)
+    Map.put(scoring, :stats, [{player, 1000}])
+  end
+
+  defp do_update(:send_winner_message, scoring) do
+    winning_hand =
+      Enum.filter(scoring.hands, fn hand ->
+        {_, score} = Enum.max_by(scoring.stats, fn {_, score} -> score end)
+        hand.score == score
+      end)
+      |> hd()
+
+    Events.present_winning_hand(
+      scoring.game_id,
+      winning_hand.best_hand,
+      hd(scoring.winners),
+      winning_hand.type_string
+    )
+
+    %{scoring | winning_hand: winning_hand}
+  end
+end
