ad6cbc8cb86d3c81a357fecf516c0878883f63c1
Kayser <zkayser@gmail.com>
2018-05-20 14:08:22 -0400

    Remove unused function in PlayerView module

--- lib/poker_ex_web/views/player_view.ex
+++ lib/poker_ex_web/views/player_view.ex
@@ -53,10 +53,6 @@ defmodule PokerExWeb.PlayerView do
 
   def full_name(%Player{name: name}), do: "#{String.capitalize(name)}'s Profile"
 
-  defp paginated_entries(list) when is_list(list) do
-    Scrivener.paginate(list, page_number: 1, page_size: 10).entries
-  end
-
   def limit_pages(list) when is_list(list) do
     case Scrivener.paginate(list, page_number: 1, page_size: 10).total_pages do
       x when x < 5 -> x
