469cc46b29e446f55a7590faa5957d78379f6a21
Kayser <zkayser@gmail.com>
2018-05-20 13:54:03 -0400

    Remove noise from PageController

--- lib/poker_ex_web/controllers/page_controller.ex
+++ lib/poker_ex_web/controllers/page_controller.ex
@@ -2,14 +2,6 @@ defmodule PokerExWeb.PageController do
   use PokerExWeb, :controller
 
   def index(conn, _params) do
-    #[{pid, _}] = Registry.lookup(PokerEx.RoomRegistry, "room_1")
-    #ids = Registry.keys(PokerEx.RoomRegistry, pid)
-
-    #{_, room_states} = Enum.map_reduce(ids, %{},
-    #  fn (room, acc) ->
-    #    {{room, :state}, Map.put(acc, String.to_atom(room), PokerEx.Room.state(String.to_atom(room)))}
-    #  end)
-
-    render conn, "index.html" # rooms: Map.values(room_states)
+    render conn, "index.html"]
   end
 end
