b5a4d198af7a3fc84a8912d5e1d108046e1f50c7
Kayser <zkayser@gmail.com>
2019-07-26 16:01:02 -0400

    Add local ip to trusted clients

--- lib/poker_ex_web/endpoint.ex
+++ lib/poker_ex_web/endpoint.ex
@@ -8,7 +8,7 @@ defmodule PokerExWeb.Endpoint do
         "http://localhost:8080",
         "http://localhost:8081",
         "http://0.0.0.0:8081",
-        "http://10.20.30.194",
+        "http://10.20.30.194:8081",
         "https://ancient-forest-15148.herokuapp.com/",
         "https://poker-ex.herokuapp.com/"
       ]
@@ -46,7 +46,7 @@ defmodule PokerExWeb.Endpoint do
     origin: [
       "http://localhost:8081",
       "http://0.0.0.0:8081",
-      "http://10.20.30.194",
+      "http://10.20.30.194:8081",
       "http://phoenix-experiment-zkayser.c9users.io:8081",
       "https://poker-ex.herokuapp.com"
     ]
