d310e389827df4a03be981c0a111aa6c6c4a68af
Zack Kayser <zkayser@gmail.com>
2019-11-26 08:24:09 -0500

    Remove unused PokerEx.Players.Player behaviour

--- test/lib/players/anon_test.exs
+++ test/lib/players/anon_test.exs
@@ -23,40 +23,6 @@ defmodule PokerEx.Players.AnonTest do
     end
   end
 
-  describe "bet/2" do
-    test "takes a number of chips and subtracts that number from the player\'s chip count" do
-      {:ok, player} = Anon.new(%{"name" => "A"})
-
-      assert {:ok, %Anon{chips: 800}} = Anon.bet(player, 200)
-    end
-
-    test "only allows a player to bet up to the number of chips that they have" do
-      {:ok, player} = Anon.new(%{"name" => "A"})
-
-      assert {:ok, %Anon{chips: 0}} = Anon.bet(player, 100_000)
-    end
-
-    test "does not allow players to bet negative amounts" do
-      {:ok, player} = Anon.new(%{"name" => "A"})
-
-      assert :error = Anon.bet(player, -2000)
-    end
-  end
-
-  describe "credit/2" do
-    test "adds to the player\'s chip account" do
-      {:ok, player} = Anon.new(%{"name" => "A"})
-
-      assert {:ok, %Anon{chips: 1200}} = Anon.credit(player, 200)
-    end
-
-    test "returns :error if given a negative value" do
-      {:ok, player} = Anon.new(%{"name" => "A"})
-
-      assert :error = Anon.credit(player, -1000)
-    end
-  end
-
   describe "String.Chars protocol" do
     test "returns the player's name" do
       assert "name" == "#{%Anon{name: "name"}}"
