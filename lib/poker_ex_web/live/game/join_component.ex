5c68eb95254bcae2c0872802bbe0b3af8288f8a9
Zack Kayser <zkayser@gmail.com>
2019-12-04 06:32:28 -0500

    Setup testing framework for Join Live Component

--- lib/poker_ex_web/live/game/join_component.ex
+++ lib/poker_ex_web/live/game/join_component.ex
@@ -4,4 +4,8 @@ defmodule PokerExWeb.Live.JoinComponent do
   def render(assigns) do
     Phoenix.View.render(PokerExWeb.GameView, "join.html", assigns)
   end
+
+  def handle_event("change_name", %{"name" => name}, socket) do
+    {:noreply, assign(socket, :name, name)}
+  end
 end
--- test/controllers/game_controller_test.exs
+++ test/controllers/game_controller_test.exs
@@ -1,5 +1,4 @@
 defmodule PokerExWeb.GameControllerTest do
-  alias PokerExWeb.Live.JoinComponent
   import Phoenix.LiveViewTest
   use PokerExWeb.ConnCase
 
@@ -20,6 +19,24 @@ defmodule PokerExWeb.GameControllerTest do
       {:ok, view, _live} = live(conn, "games/game_1")
 
       assert render(view) =~ "Join Game"
+    end
+  end
+
+  describe "join" do
+    @component "join"
+    test "entering a name in the name-input field populates the player's name", %{conn: conn} do
+      {:ok, view, _html} = live(conn, "games/game_1")
+      name = "some player name"
+
+      assert render_change([view, @component], :change_name, %{"name" => name}) =~ name
+    end
+
+    test "entering a name in the name-input field enables the join game button", %{conn: conn} do
+      {:ok, view, _html} = live(conn, "games/game_1")
+      name = "some player name"
+
+      html = render_change([view, @component], :change_name, %{"name" => name})
+      refute html =~ "disabled"
     end
   end
 end
